a645aee3d14ea6178856d24a823b1a80820a860b
Joachim Wilke <git@joachim-wilke.de>
2010-02-17 18:21:39 +0000

    - Fixed copyright information in all source code files

--- libpthread++/PThread++.cpp
+++ libpthread++/PThread++.cpp
@@ -1,10 +1,27 @@
 /*
  * libpthread++
  *
- * See COPYING for copyright information.
+ * Copyright (C) 2007-2010 Joachim Wilke <libpthread@joachim-wilke.de>
  *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+/*
+ * A simple thread base class
  * Bases on thread.c from VDR (www.cadsoft.de/vdr).
- * $Id: thread.c 1.64 2008/02/15 14:17:42 kls Exp $
  */
 
 #include "PThread++.h"
--- libpthread++/PThread++.h
+++ libpthread++/PThread++.h
@@ -1,10 +1,27 @@
 /*
- * thread.h: A simple thread base class
+ * libpthread++
  *
- * See COPYING for copyright information.
+ * Copyright (C) 2007-2010 Joachim Wilke <libpthread@joachim-wilke.de>
  *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+/*
+ * A simple thread base class
  * Bases on thread.h from VDR (www.cadsoft.de/vdr).
- * $Id: thread.h 1.39 2007/02/24 16:13:28 kls Exp $
  */
 
 #ifndef __PTHREADPP_H
